0ac01e6af5426eb4c04c5ea1cac78d8986370b55
Benjamin Belardi <benjamin.belardi@expleogroup.com>
2021-07-09 22:12:49 +0200

    fix : bug Login fix : nug dashboard feat : start test newBills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
@@ -9,6 +9,33 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+    })
+    describe("When I load a proof file",() => {
+      test("the extention file should be .jpg or .jpeg or .pnp",() => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        const firebase = jest.fn()
+        
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firebase,
+          localStorage : window.localStorage
+        })
+
+        const inputfile = "test.txt"
+        const file = screen.getByTestId("file")
+        const handleChangeFile = jest.fn(newBill.handleChangeFile)
+
+        file.addEventListener("change", handleChangeFile)
+        fireEvent.change(file, { target: {value : inputfile}})
+        expect(handleChangeFile).toHaveBeenCalled()
+      })
     })
   })
 })
